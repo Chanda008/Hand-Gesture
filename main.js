c15734144681f0c37e0ad81749fa953d6be65bda
Chanda008 <83400660+Chanda008@users.noreply.github.com>
2022-04-09 15:31:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 prediction1 = "";
-prediction2 = "";
 
 Webcam.set({
     width : 350,
@@ -24,4 +23,43 @@ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models
 
 function modelLoaded(){
     console.log("model has loaded")
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The prediction is " + prediction1;
+    var utterThis = new SpeachSynthesisUtterance(speak_data_1);
+    utterThis.rate = 0.5
+    synth.speak(utterThis);
+}
+
+function prediction(){
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction1 = results[0].label;
+        speak();
+        if(results[0].label == "Thumbs Up"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "Peace Sign"){
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if(results[0].label == "Vulcan Salute"){
+            document.getElementById("update_emoji").innerHTML = "&#128406;";
+        }
+        if(results[0].label == "Rock"){
+            document.getElementById("update_emoji").innerHTML = "&#129304;";
+        }
+        if(results[0].label == "Fist Bump"){
+            document.getElementById("update_emoji").innerHTML = "&#9994;";
+        }
+    }
 }
